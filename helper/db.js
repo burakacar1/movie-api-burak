42281609f67e271b5133bbea9ed4e1aba86c900e
Burak Acar <rehaburakacar1907@gmail.com>
2020-03-31 01:54:21 +0300

    travisci

--- helper/db.js
+++ helper/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose.connect('mongodb://movie_user:abcd1234@ds139436.mlab.com:39436/movie-api', { useMongoClient: true});
+  mongoose.connect('mongodb://deneme1:deneme123@ds143893.mlab.com:43893/heroku_0l7qtqhk', { useMongoClient: true});
 
   mongoose.connection.on('open', () => {
-    // console.log('MongoDB: Connected');
+     console.log('MongoDB: Connected');
   });
   mongoose.connection.on('error', (err) => {
     console.log('MongoDB: Error', err);
