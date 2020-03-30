a8b9f29369adbb4f97d4eb2c4e3235707903a1fe
Burak Acar <rehaburakacar1907@gmail.com>
2020-03-31 02:00:49 +0300

    sadkdlşas

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -113,7 +113,7 @@ describe('/api/movies tests', () => {
 	describe('/DELETE/:movie_id movie', () => {
 		it('it should DELETE a movie given by id', (done) => {
 			chai.request(server)
-				.delete('/api/movies/' + movieId)
+				.delete('/api/moviess/' + movieId)
 				.set('x-access-token', token)
 				.end((err, res) => {
 					res.should.have.status(200);
